e1a992dc39c47a1fd6b02e6bcbb98f41c7d51376
LiramGoli <83345475+LiramGoli@users.noreply.github.com>
2024-06-07 17:30:50 +0300

    בדיקה

--- Front/App.js
+++ Front/App.js
@@ -1,11 +1,12 @@
 import { SafeAreaView, StyleSheet, View } from 'react-native';
 import DashboardScreen from './src/screens/DashboardScreen';
 import { StatusBar } from 'expo-status-bar';
+import { globalStyles } from './src/utils/GlobalStyles';
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.container}>
-      <StatusBar backgroundColor="white" barStyle="dark-content" />
+    <SafeAreaView style={globalStyles.container}>
+      <StatusBar backgroundColor="#0A0A2A" barStyle="bright-content" />
       <DashboardScreen />
     </SafeAreaView>
   );
